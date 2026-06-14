8b7390db074dbc2f478d28201f821f36adb7a8c2
Kevin Ly <kevin.ly.ap@outlook.com>
2026-06-14 15:45:47 +0200

    Add Neo60 and Neo60 Cu Vial-QMK files

---
+++ vial-qmk/keyboards/neostudio/neo60/wired_ansiso/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* python3 util/vial_generate_keyboard_uid.py */
+#define VIAL_KEYBOARD_UID {0x84, 0xFC, 0xEB, 0x65, 0x4B, 0x07, 0x29, 0xC3}
---
+++ vial-qmk/keyboards/neostudio/neo60cu/wired_ansi/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* python3 util/vial_generate_keyboard_uid.py */
+#define VIAL_KEYBOARD_UID {0x30, 0x04, 0x73, 0x02, 0x04, 0x95, 0x47, 0xC5}
