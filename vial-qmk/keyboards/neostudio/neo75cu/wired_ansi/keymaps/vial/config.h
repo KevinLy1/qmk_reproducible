5c712b001740caecf99ba883cd61a67066930933
Kevin Ly <kevin.ly.ap@outlook.com>
2026-06-14 17:24:05 +0200

    Add Neo75 Cu Vial-QMK files

---
+++ vial-qmk/keyboards/neostudio/neo75cu/wired_ansi/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* python3 util/vial_generate_keyboard_uid.py */
+#define VIAL_KEYBOARD_UID {0xFB, 0xD7, 0xFC, 0xC1, 0x60, 0xB8, 0x79, 0xC6}
