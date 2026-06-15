fd2230dfbe89bd608ea0c5e9a1fd5da230d84bad
Kevin Ly <kevin.ly.ap@outlook.com>
2026-06-15 21:47:37 +0200

    Add missing config.h for Neo65 Cu Vial

---
+++ vial-qmk/keyboards/neostudio/neo65cu/wired_ansiso/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* python3 util/vial_generate_keyboard_uid.py */
+#define VIAL_KEYBOARD_UID {0xC9, 0xA3, 0x06, 0xD6, 0xAD, 0xB1, 0x68, 0xCF}
