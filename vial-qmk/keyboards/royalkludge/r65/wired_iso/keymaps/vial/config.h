d6d329a2c84dfe30efc1ccd267ca8f3a8d7b8883
Kevin Ly <kevin.ly.ap@outlook.com>
2026-06-19 01:41:16 +0200

    Add Royal Kludge R65 ISO Vial files

---
+++ vial-qmk/keyboards/royalkludge/r65/wired_iso/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* python3 util/vial_generate_keyboard_uid.py */
+#define VIAL_KEYBOARD_UID {0x25, 0x00, 0x90, 0x74, 0xFA, 0x1C, 0x71, 0x0D}
---
+++ vial-qmk/keyboards/royalkludge/r65/wired_iso/r65.c
@@ -0,0 +1,5 @@
+#include "quantum.h"
+
+#include QMK_KEYBOARD_H
+
+#define LED_ENABLE_PIN A5
