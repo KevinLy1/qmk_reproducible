196ffbb52f760e3877666bd7645916691ccaa5ad
Kevin Ly <kevin.ly.ap@outlook.com>
2026-08-26 14:00:58 +0200

    DIY CKB84 - Switch to RGB Matrix

--- vial-qmk/keyboards/diy/ckb84/keymaps/vial/keymap.c
+++ vial-qmk/keyboards/diy/ckb84/keymaps/vial/keymap.c
@@ -13,34 +13,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,    KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,  KC_F22,   KC_F23,   KC_F24,  KC_VOLD,  KC_VOLU, KC_NO,
       KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,            KC_HOME,
       KC_TAB,    KC_HOME,  KC_UP,    KC_END,   KC_DEL,   KC_INS,   KC_SCRL,  KC_PAUS,  KC_NO,    KC_HOME, KC_END,   KC_PGUP,  KC_PGDN, KC_ENT,            KC_END,
-      KC_NO,     KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PSCR,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    UG_PREV, UG_NEXT,  UG_VALD,  UG_VALU,                    KC_PGUP,
+      KC_NO,     KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PSCR,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    RM_PREV, RM_NEXT,  RM_VALD,  RM_VALU,                    KC_PGUP,
       KC_LSFT,   KC_NO,    KC_NO,    KC_CAPS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_MUTE,  KC_VOLD, KC_VOLU,  KC_RSFT,                     KC_PGUP, KC_PGDN,
       KC_LCTL,   KC_LGUI,  KC_LALT,                      KC_SPC,                       KC_RALT,  KC_RGUI, KC_RCTL,                     KC_HOME,  KC_PGDN, KC_END
     )
 };
-
-#ifdef RGBLIGHT_ENABLE
-const rgblight_segment_t PROGMEM caps_lock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {63, 1, HSV_WHITE}
-);
-
-const rgblight_segment_t PROGMEM scroll_lock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {37, 1, HSV_WHITE}
-);
-
-const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    caps_lock_layer,
-    scroll_lock_layer
-);
-
-void keyboard_post_init_user(void) {
-    rgblight_layers = rgb_layers;
-}
-
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(0, led_state.caps_lock);
-    rgblight_set_layer_state(1, led_state.scroll_lock);
-    
-    return true;
-}
-#endif
